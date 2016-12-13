05b3e715447b3d2669f122a193f4ad17d8dc7ffb
pratikso <pratik.s@cisinlabs.com>
2016-12-13 12:48:49 +0530

    Update Author

--- index.php
+++ index.php
@@ -3,9 +3,9 @@
 Plugin Name: Custom product designer
 Plugin URI: https://www.skyprotect.com
 Description: Custom product designer plugin
-Author: Tony Ryan
+Author: CIS
 Version: 1.0
-Author URI: https://www.skyprotect.com
+Author URI: https://www.cisin.com
 */
 
 define('PLUGIN_PATH', plugin_dir_path(__FILE__));
